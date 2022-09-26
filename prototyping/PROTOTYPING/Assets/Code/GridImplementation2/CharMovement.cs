c2d468f963274c931f90c8a1f25ff0d1342adb27
syrtis-m <54552425+syrtis-m@users.noreply.github.com>
2022-09-26 11:13:38 -0500

    updates for WebGL build compatibility

--- prototyping/PROTOTYPING/Assets/Code/GridImplementation2/CharMovement.cs
+++ prototyping/PROTOTYPING/Assets/Code/GridImplementation2/CharMovement.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Timeline;
+
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
